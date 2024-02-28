3fb9f975884b280552249c08fc4ff4d52ae47680
daihukumoshi <25116353+daihukumoshi@users.noreply.github.com>
2024-02-28 12:07:06 +0900

    退避

--- go/app/main.go
+++ go/app/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"crypto/sha256"
+	"database/sql"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"path"
-	"strings"
-	"io"
-	"crypto/sha256"
 	"strconv"
-	"database/sql"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -26,8 +26,8 @@ type Response struct {
 }
 
 type Item struct {
-	Name string `json:"name"`
-	Category string `json:"category"`
+	Name      string `json:"name"`
+	Category  string `json:"category"`
 	Imagename string `json:"image_name"`
 }
 
@@ -40,21 +40,21 @@ func root(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 }
 
-func sha_256(target []byte) string { 
+func sha_256(target []byte) string {
 	//ハッシュ化
-	h := sha256.New() 
-	h.Write(target) 
+	h := sha256.New()
+	h.Write(target)
 	fmt.Printf("%x", h.Sum(nil))
 	return fmt.Sprintf("%x", h.Sum(nil))
 }
 
 func handleError(c echo.Context, err error) error {
-    c.Logger().Error(err)
-    return c.JSON(http.StatusInternalServerError, Response{Message: "Internal Server Error"})
+	c.Logger().Error(err)
+	return c.JSON(http.StatusInternalServerError, Response{Message: "Internal Server Error"})
 }
 
 func openDatabase() (*sql.DB, error) {
-    return sql.Open("sqlite3", "../db/mercari.sqlite3")
+	return sql.Open("sqlite3", "./db/mercari.sqlite3")
 }
 
 func addItem(c echo.Context) error {
@@ -65,18 +65,17 @@ func addItem(c echo.Context) error {
 	message := fmt.Sprintf("item received: %s", name)
 	res := Response{Message: message}
 
-
 	//ここから画像
 	// フォームのファイルを取得
 	file, err := c.FormFile("image")
 	if err != nil {
 		return handleError(c, err)
-    }
+	}
 	//ファイルを開いて内容をsrcに代入
 	src, err := file.Open()
 	if err != nil {
 		return handleError(c, err)
-    }
+	}
 	defer src.Close()
 
 	fileModel := strings.Split(file.Filename, ".")
@@ -85,132 +84,128 @@ func addItem(c echo.Context) error {
 	fileName_hash := sha_256([]byte(fileName))
 
 	// 保存用のファイルを作成する
-	dst, err := os.Create("./images/"+fmt.Sprintf("%s.%s",string(fileName_hash),extension))
+	dst, err := os.Create("./images/" + fmt.Sprintf("%s.%s", string(fileName_hash), extension))
 	if err != nil {
 		return handleError(c, err)
-    }
+	}
 	defer dst.Close()
 
 	// アップロードされたファイルの内容を保存用のファイルにコピーする
 	_, err = io.Copy(dst, src)
 	if err != nil {
 		return handleError(c, err)
-    }
+	}
 	c.Logger().Infof("アップロード成功!")
 	// ここまで画像
 	imageName := fileName_hash + "." + extension
 
-
 	//dbに保存
-	db, err := openDatabase() 
+	db, err := openDatabase()
 	if err != nil {
-        return handleError(c, err)
-    }
-    defer db.Close() 
+		return handleError(c, err)
+	}
+	defer db.Close()
 
 	var category_id int
 	var category_name string
 
 	//カテゴリ名が一致するcategoriesのレコード取得
 	err = db.QueryRow("SELECT id, name FROM categories WHERE name = ?", category).Scan(&category_id, &category_name)
-    if err != nil {
+	if err != nil {
 		//一致するカテゴリ名がなければ作る
 		res, err := db.Exec("INSERT INTO categories (name) VALUES (?)", category)
-        if err != nil {
+		if err != nil {
 			return handleError(c, err)
-    	}
+		}
 		//uuidの取得
 		newCategoryID, err := res.LastInsertId()
 		if err != nil {
-    		return handleError(c, err)
-    	}
+			return handleError(c, err)
+		}
 		category_id = int(newCategoryID)
-    }
+	}
 
-	
 	stmt, err := db.Prepare("INSERT INTO items (name, category_id, image_name) VALUES (?, ?, ?)")
 	if err != nil {
-        return handleError(c, err)
-    }
+		return handleError(c, err)
+	}
 	defer stmt.Close()
 
-	_, err = stmt.Exec(name, category_id, imageName);
+	_, err = stmt.Exec(name, category_id, imageName)
 	if err != nil {
 		return handleError(c, err)
-    }
+	}
 
 	return c.JSON(http.StatusOK, res)
 }
 
 func showItem(c echo.Context) error {
-	db, err := sql.Open("sqlite3", "./db/mercari.sqlite3") 
+	db, err := openDatabase()
 	if err != nil {
-        return handleError(c, err)
-    }
-    defer db.Close() 
+		return handleError(c, err)
+	}
+	defer db.Close()
 
 	rows, err := db.Query("SELECT * FROM items INNER JOIN categories ON items.category_id = categories.id")
 	if err != nil {
-        return handleError(c, err)
-    }
+		return handleError(c, err)
+	}
 	defer rows.Close()
 
 	var itemsWrapper ItemsWrapper
 
 	for rows.Next() {
 		var itemId int
-        var itemName string
-        var category_id int
-        var image_name string
+		var itemName string
+		var category_id int
+		var image_name string
 		var categoryID int
 		var categoryName string
 
-        err = rows.Scan(&itemId, &itemName, &category_id, &image_name, &categoryID, &categoryName)
-        if err != nil {
+		err = rows.Scan(&itemId, &itemName, &category_id, &image_name, &categoryID, &categoryName)
+		if err != nil {
 			return handleError(c, err)
-    	}
+		}
 
-		
 		newItem := Item{Name: itemName, Category: categoryName, Imagename: image_name}
 		itemsWrapper.Items = append(itemsWrapper.Items, newItem)
-    }
+	}
 	return c.JSON(http.StatusOK, itemsWrapper)
 }
 
 func searchItem(c echo.Context) error {
 	keyword := c.QueryParam("keyword")
 
-	db, err := openDatabase() 
+	db, err := openDatabase()
 	if err != nil {
-        return handleError(c, err)
-    }
-    defer db.Close() 
+		return handleError(c, err)
+	}
+	defer db.Close()
 
 	rows, err := db.Query("SELECT * FROM items INNER JOIN categories ON items.category_id = categories.id WHERE items.name LIKE ?", "%"+keyword+"%")
 	if err != nil {
-        return handleError(c, err)
-    }
+		return handleError(c, err)
+	}
 	defer rows.Close()
-
 
 	var itemsWrapper ItemsWrapper
 
 	for rows.Next() {
 		var itemId int
-        var itemName string
-        var category_id int
-        var image_name string
+		var itemName string
+		var category_id int
+		var image_name string
 		var categoryID int
 		var categoryName string
 
-        err = rows.Scan(&itemId, &itemName, &category_id, &image_name, &categoryID, &categoryName)
-        if err != nil {
+		err = rows.Scan(&itemId, &itemName, &category_id, &image_name, &categoryID, &categoryName)
+		if err != nil {
 			return handleError(c, err)
-    	}
+		}
 
 		newItem := Item{Name: itemName, Category: categoryName, Imagename: image_name}
 		itemsWrapper.Items = append(itemsWrapper.Items, newItem)
-    }
+	}
 	return c.JSON(http.StatusOK, itemsWrapper)
 }
 
@@ -233,41 +228,41 @@ func getImg(c echo.Context) error {
 }
 
 func getItems(c echo.Context) error {
-	db, err := openDatabase() 
+	db, err := openDatabase()
 	if err != nil {
-        return handleError(c, err)
-    }
-    defer db.Close() 
+		return handleError(c, err)
+	}
+	defer db.Close()
 
 	itemID, err := strconv.Atoi(c.Param("item_id"))
 	if err != nil {
-    	return handleError(c, err)
-    }
+		return handleError(c, err)
+	}
 
 	rows, err := db.Query("SELECT * FROM items INNER JOIN categories ON items.category_id = categories.id WHERE items.id=?", itemID)
 	if err != nil {
-        return handleError(c, err)
-    }
+		return handleError(c, err)
+	}
 	defer rows.Close()
 
 	var itemsWrapper ItemsWrapper
 
 	for rows.Next() {
 		var itemId int
-        var itemName string
-        var category_id int
-        var image_name string
+		var itemName string
+		var category_id int
+		var image_name string
 		var categoryID int
 		var categoryName string
 
-        err = rows.Scan(&itemId, &itemName, &category_id, &image_name, &categoryID, &categoryName)
-        if err != nil {
+		err = rows.Scan(&itemId, &itemName, &category_id, &image_name, &categoryID, &categoryName)
+		if err != nil {
 			return handleError(c, err)
-    	}
+		}
 
 		newItem := Item{Name: itemName, Category: categoryName, Imagename: image_name}
 		itemsWrapper.Items = append(itemsWrapper.Items, newItem)
-    }
+	}
 
 	return c.JSON(http.StatusOK, itemsWrapper)
 }
@@ -300,4 +295,3 @@ func main() {
 	// Start server
 	e.Logger.Fatal(e.Start(":9000"))
 }
-
